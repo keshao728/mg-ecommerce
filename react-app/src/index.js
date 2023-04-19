0ce942dc2e60a5b65ed27a69db7adcae4440cec7
Kelly <kellyshao728@gmail.com>
2023-04-19 02:05:53 -0700

    Configure a React project for Stripe Payments

---
+++ react-app/src/index.js
@@ -0,0 +1,15 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+export const stripePromise = loadStripe(
+  'pk_test_51MyVNvAOU2X4jYwWGbrdJkzZhQifsqQbw1xaRDiWpusK0ys7i4mGdRURGEn5yBxXv36agutbXaEeDkLMjeDaEK2g00BFdJnGdE'
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Elements stripe={stripePromise}>
+        <App />
+    </Elements>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
