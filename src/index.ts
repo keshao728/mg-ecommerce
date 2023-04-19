e0ae28e97c01ffc14f5de4a48c7822c589cc3bb3
Kelly <kellyshao728@gmail.com>
2023-04-19 01:28:21 -0700

    Configure Node.js for hot reloading with TypeScript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+
+if(process.env.NODE_ENV !== "production") {
+  config();
+}
+
+import Stripe from "stripe";
+export const stripe = new Stripe(process.env.STRIPE_SECRET, {
+  apiVersion: "2020-08-27",
+});
