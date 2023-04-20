41cbae5b6cc2bc612bccabd7ba486fc87cee869e
Kelly <kellyshao728@gmail.com>
2023-04-19 17:41:30 -0700

    Create a checkout session on the server

--- server/lib/api.js
+++ server/lib/api.js
@@ -5,13 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
+const checkout_1 = require("./checkout");
 exports.app = (0, express_1.default)();
-// Allows cross origin requests
-const cors_1 = __importDefault(require("cors"));
-exports.app.use((0, cors_1.default)({ origin: true }));
-exports.app.use(express_1.default.json());
-exports.app.post("/test", (req, res) => {
-    const amount = req.body.amount;
-    res.status(200).send({ with_tax: amount * 7 });
-});
+/**
+ * Catch async errors when awaiting promises
+ */
+function runAsync(callback) {
+    return (req, res, next) => {
+        callback(req, res, next).catch(next);
+    };
+}
+/**
+ * Checkouts
+ */
+exports.app.post("/checkouts/", runAsync(async ({ body }, res) => {
+    res.send(await (0, checkout_1.createStripeCheckoutSession)(body.line_items));
+}));
 //# sourceMappingURL=api.js.map
--- server/src/api.ts
+++ server/src/api.ts
@@ -1,13 +1,22 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import { createStripeCheckoutSession } from "./checkout";
 export const app = express();
 
-// Allows cross origin requests
-import cors from "cors";
-app.use(cors({ origin: true }));
+/**
+ * Catch async errors when awaiting promises
+ */
+function runAsync(callback: Function) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    callback(req, res, next).catch(next);
+  };
+}
 
-app.use(express.json());
-
-app.post("/test", (req: Request, res: Response) => {
-  const amount = req.body.amount;
-  res.status(200).send({ with_tax: amount * 7 });
-});
+/**
+ * Checkouts
+ */
+app.post(
+  "/checkouts/",
+  runAsync(async ({ body }: Request, res: Response) => {
+    res.send(await createStripeCheckoutSession(body.line_items));
+  })
+);
